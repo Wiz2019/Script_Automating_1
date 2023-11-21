ace90b498a3479b84e735b6a89fac74b628c644d
Wiz <1yashagrawal@gmail.com>
2023-11-21 15:22:01 +0530

    skips the non image file

--- Imageautomation.py
+++ Imageautomation.py
@@ -1,18 +1,22 @@
-from PIL import Image, ImageEnchance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path = './imgs'
-pathOut = '/editedImgs'
+path = r'C:\Users\1yash\Pictures\Image automated script _original'
+pathOut = r'C:\Users\1yash\Pictures\Image automated script'
 
 for filename in os.listdir(path):
-    img = Image.open(f"{path}/{filename}")
+    try:
+        img = Image.open(f"{path}/{filename}")
+    except (IOError, OSError, Image.UnidentifiedImageError):
+        # Skip non-image files
+        continue
 
     edit = img.filter(ImageFilter.SHARPEN).convert('L')
 
     factor = 1.5
-    enhancer = ImageEnchance.Contrast(edit)
+    enhancer = ImageEnhance.Contrast(edit)
     edit = enhancer.enhance(factor)
 
     clean_name = os.path.splitext(filename)[0]
 
-    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
+    edit.save(f'{pathOut}/{clean_name}_edited.jpg')
